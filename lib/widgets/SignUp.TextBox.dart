34e2e3a0f28fecb2fe39eefac20310c0808f469c
Six_Tail <qowlgh1215@naver.com>
2024-05-29 02:39:03 +0900

    [이메일 로그인 구현 및 유효성 추가]

--- lib/widgets/SignUp.TextBox.dart
+++ lib/widgets/SignUp.TextBox.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:todobest_home/screen/Calender.Screen.dart';
 import 'package:todobest_home/screen/Login.Screen.dart';
 
 import '../utils/Main.Colors.dart';
