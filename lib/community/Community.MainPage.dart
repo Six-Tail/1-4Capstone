b0f68561052a07e5be6b86a4e33face3e6f6c752
ycs-202007034 <heesup9683@naver.com>
2024-11-01 14:43:54 +0900

    커뮤니티 메인 스크롤 오류 수정 및 ui변경

--- lib/community/Community.MainPage.dart
+++ lib/community/Community.MainPage.dart
@@ -47,242 +47,251 @@ class _CommunityMainPageState extends State<CommunityMainPage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // 첫 번째 Row: 기존의 두 개의 박스
-            Row(
-              children: [
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      setState(() {
-                        isHotSelected = true;
-                        isMyPostsSelected = false;
-                      });
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HotBoardScreen(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: isHotSelected ? Colors.brown.shade100 : Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: Colors.grey),
-                      ),
-                      child: const Column(
-                        children: [
-                          Icon(Icons.local_fire_department, size: 40, color: Colors.deepOrange),
-                          SizedBox(height: 30),
-                          Text('HOT 게시판', style: TextStyle(fontSize: 16)),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      setState(() {
-                        isHotSelected = false;
-                        isMyPostsSelected = true;
-                      });
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => WroteBoardScreen(userId: ''),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: isMyPostsSelected ? Colors.brown.shade100 : Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: Colors.grey),
-                      ),
-                      child: const Column(
-                        children: [
-                          Icon(Icons.edit_note_sharp, size: 40, color: Colors.blueAccent),
-                          SizedBox(height: 30),
-                          Text('내가 쓴 글', style: TextStyle(fontSize: 16)),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-
-            // 두 번째 Row: 세 번째 박스는 AllPostsScreen로 이동, 네 번째 박스는 ScrapPage로 이동
-            Row(
-              children: [
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      // AllPostsScreen 페이지로 이동
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AllPostsScreen(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: Colors.grey),
-                      ),
-                      child: const Column(
-                        children: [
-                          Icon(Icons.paste, size: 40, color: Colors.purple),
-                          SizedBox(height: 30),
-                          Text('전체 글', style: TextStyle(fontSize: 16)),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ScrapPage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: Colors.grey),
-                      ),
-                      child: const Column(
-                        children: [
-                          Icon(Icons.star_border_outlined, size: 40, color: Colors.yellow),
-                          SizedBox(height: 30),
-                          Text('스크랩', style: TextStyle(fontSize: 16)),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CommentedPostsPage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: Colors.grey),
-                      ),
-                      child: const Column(
-                        children: [
-                          Icon(Icons.comment, size: 40, color: Colors.teal),
-                          SizedBox(height: 30),
-                          Text('댓글 단 글', style: TextStyle(fontSize: 16)),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-
-            // 기존의 확장형 게시판 목록
-            Theme(
-              data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-              child: ExpansionTile(
-                title: const Text(
-                  '게시판',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                initiallyExpanded: true,
-                tilePadding: const EdgeInsets.symmetric(horizontal: 0),
-                childrenPadding: const EdgeInsets.symmetric(horizontal: 0),
+      body: SingleChildScrollView( // 스크롤 가능하게 만듭니다.
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // 첫 번째 Row: 기존의 두 개의 박스
+              Row(
                 children: [
-                  ListTile(
-                    title: const Text('자유 게시판'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FreeBoardScreen(),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          isHotSelected = true;
+                          isMyPostsSelected = false;
+                        });
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HotBoardScreen(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.grey, width: 2),
                         ),
-                      );
-                    },
+                        child: const Column(
+                          children: [
+                            Icon(Icons.local_fire_department, size: 40, color: Colors.deepOrange),
+                            SizedBox(height: 30),
+                            Text('HOT 게시판', style: TextStyle(fontSize: 16,
+                              fontWeight: FontWeight.bold,)),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                  ListTile(
-                    title: const Text('목표 공유 게시판'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => GoalShareBoardScreen(),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          isHotSelected = false;
+                          isMyPostsSelected = true;
+                        });
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => WroteBoardScreen(userId: ''),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.grey, width: 2),
                         ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('자기계발 팁 게시판'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SdTipBoardScreen(),
+                        child: const Column(
+                          children: [
+                            Icon(Icons.edit_note_sharp, size: 40, color: Colors.blueAccent),
+                            SizedBox(height: 30),
+                            Text('내가 쓴 글', style: TextStyle(fontSize: 16,
+                              fontWeight: FontWeight.bold,)),
+                          ],
                         ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('멘토링 요청 게시판'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MentoringBoardScreen(),
-                        ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('홍보 게시판'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PromotionBoardScreen(),
-                        ),
-                      );
-                    },
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+              const SizedBox(height: 16),
+
+              // 두 번째 Row: 세 번째 박스는 AllPostsScreen로 이동, 네 번째 박스는 ScrapPage로 이동
+              Row(
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        // AllPostsScreen 페이지로 이동
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AllPostsScreen(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.grey, width: 2),
+                        ),
+                        child: const Column(
+                          children: [
+                            Icon(Icons.paste, size: 40, color: Colors.purple),
+                            SizedBox(height: 30),
+                            Text('전체 글',
+                                style: TextStyle(fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ScrapPage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.grey, width: 2),
+                        ),
+                        child: const Column(
+                          children: [
+                            Icon(Icons.star_border_outlined, size: 40, color: Colors.yellow),
+                            SizedBox(height: 30),
+                            Text('스크랩', style: TextStyle(fontSize: 16,
+                              fontWeight: FontWeight.bold,)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CommentedPostsPage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.grey, width: 2),
+                        ),
+                        child: const Column(
+                          children: [
+                            Icon(Icons.comment, size: 40, color: Colors.teal),
+                            SizedBox(height: 30),
+                            Text('댓글 단 글', style: TextStyle(fontSize: 16,
+                              fontWeight: FontWeight.bold,)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+
+              // 기존의 확장형 게시판 목록
+              Theme(
+                data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+                child: ExpansionTile(
+                  title: const Text(
+                    '게시판',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  initiallyExpanded: true,
+                  tilePadding: const EdgeInsets.symmetric(horizontal: 0),
+                  childrenPadding: const EdgeInsets.symmetric(horizontal: 0),
+                  children: [
+                    ListTile(
+                      title: const Text('자유 게시판'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FreeBoardScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('목표 공유 게시판'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => GoalShareBoardScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('자기계발 팁 게시판'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SdTipBoardScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('멘토링 요청 게시판'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MentoringBoardScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('홍보 게시판'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PromotionBoardScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
